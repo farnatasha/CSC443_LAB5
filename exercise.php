8fb20d7b8191d1eb304e13ad9110c7dffa84bc65
farnatasha <2020898386@student.uitm.edu.my>
2022-05-09 11:18:13 +0800

    3 commit

--- exercise.php
+++ exercise.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <?php
-    $monthDays = array ('Splorch' => 23, 'Sploo' => 28,  'Splat' => v2, 'Splatt' => 3,  'Spleen' => 44, 'Splune' => 30,   'Spling' => 61, 'Slendo' => 61, 'Sploctember' => 31, 'Splictember' => 31, 
+    $monthDays = array ('Splorch' => 23, 'Sploo' => 28,  'Splat' => 2, 'Splatt' => 3,  'Spleen' => 44, 'Splune' => 30,   'Spling' => 61, 'Slendo' => 61, 'Sploctember' => 31, 'Splictember' => 31, 
     'Splanet' => 30, 'TheRest' => 22);
    // shortest days 
    echo "MINIMUM DAYS :";
